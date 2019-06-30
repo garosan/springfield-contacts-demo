6a5ee93d503c040e982a43b8bd892d4c74d998b6
Garo San <garosanchez27@gmail.com>
2019-06-30 17:25:55 -0500

    Updates

---
+++ precache-manifest.7644de5327650e1966052dd7a573fe40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37ccc687be6041b053b302f03144f2a",
+    "url": "/springfield-contacts-demo/index.html"
+  },
+  {
+    "revision": "540868164ec2bce4f655",
+    "url": "/springfield-contacts-demo/static/css/main.f94260e7.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/springfield-contacts-demo/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "540868164ec2bce4f655",
+    "url": "/springfield-contacts-demo/static/js/main.c6314e92.chunk.js"
+  },
+  {
+    "revision": "fffb15e81fa07bf29e8a",
+    "url": "/springfield-contacts-demo/static/js/runtime~main.b30764b8.js"
+  }
+]);
